22dd8c62c284b726a7a702d582f765ddf4449b2d
yuhongliang324 <yuhongliang324@gmail.com>
2017-02-20 16:32:44 -0500

    try

--- assign1/yhl/try_load.py
+++ assign1/yhl/try_load.py
@@ -5,9 +5,9 @@ import dynet
 
 def load(fn):
     model = dynet.Model()
-    ret = model.load(fn)
-    print ret
-    return model.load(fn), model
+    model.load(fn)
+    print model
+    return model
 
 
 load('../models/LSTM_epoch_4_layer2_hidden_128_embed_150_att_128_02-20-16-59-09')
